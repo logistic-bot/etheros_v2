a207795817b35cdb55a97c51c8468fe00d6d0395
Khais COLIN <logistic-bot@protonmail.com>
2021-03-04 13:52:35 +0100

    9: Loop infinitly on kernel exit

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -9,12 +9,10 @@ extern "C" void _start(BootInfo *bootInfo)
 
     BasicRenderer renderer = BasicRenderer(bootInfo->framebuffer, bootInfo->psf1_font);
 
-    pageTableManager->map_memory((void*)0x600000000, (void*)0x80000);
-    uint64_t* test = (uint64_t*)0x600000000;
-    *test = 26;
-    renderer.print(to_string(*test));
+    renderer.print("Kernel Initialized Sucessfully");
 
-    return;
+    // prevent crash on real hardware
+    while(true);
 }
 
 // KNOWN BUG (MAYBE RESOLVED): (with 256M of memory) when allocating 56944 pages, GetFreeRAM() returns stranges, waytoobig values (> 10000000000). OOM happens at 56952 pages.
