b00c88055ed4f68acf61be7925b9b3a703be35ad
Khais COLIN <logistic-bot@protonmail.com>
2021-03-04 13:25:22 +0100

    9: Cleaning kernel.cpp with kernelUtil.h: Moving imports and structs

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,25 +1,4 @@
-﻿#include <stddef.h>
-#include "BasicRenderer.h"
-#include "cstr.h"
-#include "efiMemory.h"
-#include "memory.h"
-#include "Bitmap.h"
-#include "paging/PageFrameAllocator.h"
-#include "paging/PageMapIndexer.h"
-#include "paging/PageTableManager.h"
-#include "paging/paging.h"
-
-struct BootInfo
-{
-    Framebuffer *framebuffer;
-    PSF1_FONT *psf1_font;
-    EFI_MEMORY_DESCRIPTOR *mMap;
-    uint64_t mMapSize;
-    uint64_t mMapDescSize;
-};
-
-extern uint64_t _KernelStart;
-extern uint64_t _KernelEnd;
+﻿#include "kernelUtil.h"
 
 extern "C" void _start(BootInfo *bootInfo)
 {
--- src/kernelUtil.h
+++ src/kernelUtil.h
@@ -1,4 +1,25 @@
-#ifndef KERNELUTIL_H
-#define KERNELUTIL_H
+﻿#pragma once
 
-#endif // KERNELUTIL_H
+#include <stddef.h>
+#include "BasicRenderer.h"
+#include "cstr.h"
+#include "efiMemory.h"
+#include "memory.h"
+#include "Bitmap.h"
+#include "paging/PageFrameAllocator.h"
+#include "paging/PageMapIndexer.h"
+#include "paging/PageTableManager.h"
+#include "paging/paging.h"
+
+struct BootInfo
+{
+    Framebuffer *framebuffer;
+    PSF1_FONT *psf1_font;
+    EFI_MEMORY_DESCRIPTOR *mMap;
+    uint64_t mMapSize;
+    uint64_t mMapDescSize;
+};
+
+extern uint64_t _KernelStart;
+extern uint64_t _KernelEnd;
+
